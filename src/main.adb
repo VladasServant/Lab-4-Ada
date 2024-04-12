8d346bb9da9df435debe5d8d4aecbe859c349371
Lolgixpro <95712610+VladasServant@users.noreply.github.com>
2024-04-12 10:47:56 +0300

    Update main.adb

--- src/main.adb
+++ src/main.adb
@@ -7,38 +7,49 @@ procedure main is
       entry Start(Id : Integer);
    end Phylosopher;
 
-   Forks : array (1..10) of Counting_Semaphore(1, Default_Ceiling);
+   Forks : array (1..5) of Counting_Semaphore(1, Default_Ceiling);
 
    task body Phylosopher is
-      Id : Integer;
-      Id_Left_Fork, Id_Right_Fork : Integer;
-   begin
-      accept Start (Id : in Integer) do
-         Phylosopher.Id := Id;
-         Id_Left_Fork := Id;
-         Id_Right_Fork := Id rem 5 + 1;
-      end Start;
+    Id : Integer;
+    Id_Left_Fork, Id_Right_Fork : Integer;
+    Is_Even : Boolean;
+begin
+    accept Start (Id : in Integer) do
+        Phylosopher.Id := Id;
+        Id_Left_Fork := Id;
+        Id_Right_Fork := Id rem 5 + 1;
+        Is_Even := (Id mod 2 = 0);
+    end Start;
 
     for I in 1..10 loop
-      Put_Line("Phylosopher " & Id'Img & " thinking ");
+        Put_Line("Phylosopher " & Id'Img & " thinking ");
+        
+        if Is_Even then
+            Forks(Id_Left_Fork).Seize;
+            Put_Line("Phylosopher " & Id'Img & " took left fork");
+            
+            Forks(Id_Right_Fork).Seize;
+            Put_Line("Phylosopher " & Id'Img & " took right fork");
+        else
+            Forks(Id_Right_Fork).Seize;
+            Put_Line("Phylosopher " & Id'Img & " took right fork");
+            
+            Forks(Id_Left_Fork).Seize;
+            Put_Line("Phylosopher " & Id'Img & " took left fork");
+        end if;
 
-      Forks(Id_Left_Fork).Seize;
-      Put_Line("Phylosopher " & Id'Img & " took left fork");
-
-      Forks(Id_Right_Fork).Seize;
-      Put_Line("Phylosopher " & Id'Img & " took right fork");
-
-      Put_Line("Phylosopher " & Id'Img & " eating");
-
-      Forks(Id_Right_Fork).Release;
-      Put_Line("Phylosopher " & Id'Img & " put right fork");
-
-      Forks(Id_Left_Fork).Release;
-      Put_Line("Phylosopher " & Id'Img & " put left fork");
+        Put_Line("Phylosopher " & Id'Img & " eating");
+        
+        Forks(Id_Right_Fork).Release;
+        Put_Line("Phylosopher " & Id'Img & " put right fork");
+        
+        Forks(Id_Left_Fork).Release;
+        Put_Line("Phylosopher " & Id'Img & " put left fork");
     end loop;
-   end Phylosopher;
+end Phylosopher;
 
-   Phylosophers : array (1..10) of Phylosopher;
+
+   Phylosophers : array (1..5) of Phylosopher;
 Begin
    for I in Phylosophers'Range loop
       Phylosophers(I).Start(I);
